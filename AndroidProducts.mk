4d955d53e4e3c4065d80c79eed9e76022900ff5b
NurKeinNeid <mralexman3000@gmail.com>
2020-11-28 20:36:21 +0100

    dumpling: Derpify
    Change-Id: If1307d484602f11533aab15225352ea49e1df808
    Signed-off-by: NurKeinNeid <mralexman3000@gmail.com>

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -15,9 +15,9 @@
 #
 
 PRODUCT_MAKEFILES := \
-    $(LOCAL_DIR)/lineage_dumpling.mk
+    $(LOCAL_DIR)/derp_dumpling.mk
 
 COMMON_LUNCH_CHOICES := \
-    lineage_dumpling-user \
-    lineage_dumpling-userdebug \
-    lineage_dumpling-eng
+    derp_dumpling-user \
+    derp_dumpling-userdebug \
+    derp_dumpling-eng
