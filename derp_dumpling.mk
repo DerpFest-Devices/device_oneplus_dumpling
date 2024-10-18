a43221817ae8ae6b278c016ea0f18f6c0a5afcb9
LuK1337 <priv.luk@gmail.com>
2024-10-18 11:40:33 +0200

    dumpling: Adjust override prop key names
    Change-Id: I436bbf425023dbe54cf3e2c35f50a5575511bc38

--- derp_dumpling.mk
+++ derp_dumpling.mk
@@ -32,16 +32,12 @@ PRODUCT_MODEL := ONEPLUS A5010
 
 PRODUCT_GMS_CLIENTID_BASE := android-oneplus
 
-PRODUCT_SYSTEM_DEVICE := OnePlus5T
-PRODUCT_SYSTEM_NAME := OnePlus5T
-
 PRODUCT_BUILD_PROP_OVERRIDES += \
-    TARGET_DEVICE=OnePlus5T \
-    TARGET_PRODUCT=OnePlus5T
-
-PRODUCT_BUILD_PROP_OVERRIDES += \
-    PRIVATE_BUILD_DESC="OnePlus5T-user 10 QKQ1.191014.012 2010292059 release-keys"
-
-BUILD_FINGERPRINT := OnePlus/OnePlus5T/OnePlus5T:10/QKQ1.191014.012/2010292059:user/release-keys
+    BuildDesc="OnePlus5T-user 10 QKQ1.191014.012 2010292059 release-keys" \
+    BuildFingerprint=OnePlus/OnePlus5T/OnePlus5T:10/QKQ1.191014.012/2010292059:user/release-keys \
+    DeviceName=OnePlus5T \
+    DeviceProduct=OnePlus5T \
+    SystemDevice=OnePlus5T \
+    SystemName=OnePlus5T
 
 TARGET_VENDOR := oneplus
